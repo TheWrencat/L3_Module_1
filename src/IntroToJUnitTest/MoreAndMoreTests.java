dc1a70c1c9ba46e11e93d63eff7993789707ffdc
TheWrencat <league@WTS-iMac-11.attlocal.net>
2018-01-25 19:58:00 -0800

    started test testing test test

--- src/IntroToJUnitTest/MoreAndMoreTests.java
+++ src/IntroToJUnitTest/MoreAndMoreTests.java
@@ -10,25 +10,41 @@ import org.junit.Test;
 
 public class MoreAndMoreTests {
 
-//	@Test
-//	  public void MultTest() {
-//
-//	    assertEquals("10 x 0 = 0", multiply(10, 0));
-//	    assertEquals("10 x 10 = 100", multiply(10, 10));
-//	    assertEquals("8 x 11 = 88", multiply(8, 11));
-//	  }
-//
-//	@Test
-//	  public void PrimeTest() {
-//
-//	    assertTrue(isPrime(3));
-//	    assertTrue(isPrime(5));
-//	    assertTrue(isPrime(541));
-//	    assertFalse(isPrime(4));
-//	    assertFalse(isPrime(12));
-//	    assertFalse(isPrime(527));
-//
-//	  }
+	@Test
+	  public void MultTest() {
+
+	    assertEquals("10 x 0 = 0", multiply(10, 0));
+	    assertEquals("10 x 10 = 100", multiply(10, 10));
+	    assertEquals("8 x 11 = 88", multiply(8, 11));
+	  }
+
+	private Object multiply(int i, int j) {
+		int answer = i*j;
+		return i + " x " + j + " = " +answer;
+	}
+
+	@Test
+	  public void PrimeTest() {
+
+	    assertTrue(isPrime(3));
+	    assertTrue(isPrime(5));
+	    assertTrue(isPrime(541));
+	    assertFalse(isPrime(4));
+	    assertFalse(isPrime(12));
+	    assertFalse(isPrime(527));
+	  }
+
+	private boolean isPrime(int i) {
+		for(int j = 2; j<i; j++) {
+			if(i%j==0) {
+			return false;
+			}
+		}
+		return true;
+		
+	}
+	
+	
 //
 //	@Test
 //	  public void SquareTest() {
