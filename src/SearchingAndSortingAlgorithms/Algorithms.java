e0cb9ced60ec9ecf8512993b7ddaea9fabaf4061
TheWrencat <league@WTS19.local>
2018-02-08 19:44:47 -0800

    finished algorithms and tests

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -45,10 +45,55 @@ public class Algorithms {
 	public static Boolean containsSOS(List<String>message) {
 		
 		for(int i = 0; i < message.size(); i++) {
-			if(message.get(i).equals("...---...")) {
+			if(message.get(i).equals(" ... --- ... ")) {
 				return true;
 			}
 		}return false;
+	}
+	
+	public static List<Double> sortScores(List<Double>results) {
+		for(int i = 0; i<results.size(); i++) {
+			for(int j = i+1; j<results.size(); j++) {
+				if(results.get(i) > results.get(j)) {
+					Double large = results.get(i);
+					Double small = results.get(j);
+					results.set(i, small);
+					results.set(j, large);
+				}
+			}
+		}
+		System.out.println(results);
+		return results;
+	}
+	
+	public static List<String> sortDNA(List<String>sequences){
+		for(int i = 0; i<sequences.size(); i++) {
+			for(int j = i+1; j<sequences.size(); j++) {
+				if(sequences.get(i).length() > sequences.get(j).length()) {
+					String large = sequences.get(i);
+					String small = sequences.get(j);
+					sequences.set(i, small);
+					sequences.set(j, large);
+				}
+			}
+		}
+		System.out.println(sequences);
+		return sequences;
+	}
+	
+	public static List<String> sortWords(List<String>words){
+		for(int i = 0; i<words.size(); i++) {
+			for(int j = i+1; j<words.size(); j++) {
+				if(words.get(i).compareTo(words.get(j)) > 0) {
+					String small = words.get(i);
+					String large = words.get(j);
+					words.set(i, large);
+					words.set(j, small);
+				}
+			}
+		}
+		System.out.println(words);
+		return words;
 	}
 	
 	//Add other methods here
